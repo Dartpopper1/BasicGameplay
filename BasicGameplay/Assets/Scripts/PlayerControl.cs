22913c0d345dae5a84a95291a6bcda3d1ea5fc45
Dartpopper1 <espinozaj@roverkids.org>
2024-12-03 14:16:57 -0500

    Finished 2.2

--- BasicGameplay/Assets/Scripts/PlayerControl.cs
+++ BasicGameplay/Assets/Scripts/PlayerControl.cs
@@ -6,24 +6,31 @@ public class PlayerControl : MonoBehaviour
     public float horizontailInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject playerProjectile;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
         horizontailInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontailInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(playerProjectile, transform.position, playerProjectile.transform.rotation);
+        }
+
     }
 }
